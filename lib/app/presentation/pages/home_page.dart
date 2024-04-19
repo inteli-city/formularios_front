9bdd9d60c17ed67ba79f1efcc800cb069b92fe76
arimayuji <yujiarima@hotmail.com>
2024-04-19 13:21:34 -0300

    home_page ui

--- lib/app/presentation/pages/home_page.dart
+++ lib/app/presentation/pages/home_page.dart
@@ -6,6 +6,7 @@ import 'package:formularios_front/app/presentation/stores/providers/form_user_pr
 import 'package:formularios_front/app/presentation/widgets/bottom_navigation_widget.dart';
 import 'package:formularios_front/app/presentation/widgets/form_card.dart';
 import 'package:formularios_front/app/presentation/widgets/order_tab_section.dart';
+import 'package:formularios_front/app/shared/themes/app_dimensions.dart';
 import 'package:provider/provider.dart';
 
 class HomePage extends StatefulWidget {
@@ -27,31 +28,41 @@ class _HomePageState extends State<HomePage> {
               : const CircularProgressIndicator(),
       bottomNavigationBar: const BottomNavigationWidget(),
       floatingActionButton: FloatingActionButton(
-        elevation: 0,
-        shape: const CircleBorder(),
-        onPressed: () {},
-        child: const Icon(
-          Icons.abc,
-        ),
-      ),
-      floatingActionButtonLocation:
-          FloatingActionButtonLocation.miniCenterDocked,
+          elevation: 15,
+          hoverColor: Theme.of(context).colorScheme.secondary,
+          shape: const CircleBorder(),
+          onPressed: () {},
+          child: IconButton(
+            icon: const Icon(Icons.rotate_right),
+            onPressed: () {},
+          )),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 
   Widget successBuild(List<FormEntity> forms) {
-    return Column(
-      children: [
-        const OrderTabSection(),
-        Expanded(
-          child: ListView.builder(
-            itemCount: forms.length,
-            itemBuilder: (context, index) => FormCard(
-              form: forms[index],
-            ),
+    return Padding(
+      padding: const EdgeInsets.all(AppDimensions.paddingSmall),
+      child: Column(
+        children: [
+          OrderTabSection(),
+          const SizedBox(
+            height: AppDimensions.verticalSpaceExtraLarge,
           ),
-        )
-      ],
+          Expanded(
+            child: ListView.separated(
+              physics: const BouncingScrollPhysics(),
+              itemCount: forms.length,
+              itemBuilder: (context, index) => FormCard(
+                form: forms[index],
+              ),
+              separatorBuilder: (context, index) => const Divider(
+                thickness: 0,
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 

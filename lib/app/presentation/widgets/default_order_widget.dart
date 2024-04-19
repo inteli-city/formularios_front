c8f4e4503b3795b9b3ba7061e73b38962feb6253
arimayuji <yujiarima@hotmail.com>
2024-04-19 13:22:47 -0300

    default_order_widget

--- lib/app/presentation/widgets/default_order_widget.dart
+++ lib/app/presentation/widgets/default_order_widget.dart
@@ -1,24 +1,83 @@
 import 'package:flutter/material.dart';
-import 'package:formularios_front/app/shared/themes/app_colors.dart';
+import 'package:formularios_front/app/shared/helpers/utils/screen_helper.dart';
 import 'package:formularios_front/app/shared/themes/app_dimensions.dart';
 import 'package:formularios_front/app/shared/themes/app_text_styles.dart';
 
-class DefaultOrderWidget extends StatelessWidget {
+class DefaultOrderWidget extends StatefulWidget {
   final String defaultOrderTitle;
-  const DefaultOrderWidget({super.key, required this.defaultOrderTitle});
+  final int count;
+
+  const DefaultOrderWidget({
+    super.key,
+    required this.defaultOrderTitle,
+    required this.count,
+  });
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _DefaultOrderWidgetState createState() => _DefaultOrderWidgetState();
+}
+
+class _DefaultOrderWidgetState extends State<DefaultOrderWidget>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _scaleAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 100),
+      vsync: this,
+    );
+    _scaleAnimation = Tween<double>(begin: 1.0, end: 0.9).animate(_controller);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _onTapDown(TapDownDetails details) {
+    _controller.forward();
+  }
+
+  void _onTapUp(TapUpDetails details) {
+    _controller.reverse();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.symmetric(
-          horizontal: AppDimensions.marginSmall, vertical: 0),
-      elevation: Theme.of(context).cardTheme.elevation,
-      shape: Theme.of(context).cardTheme.shape,
-      child: Padding(
-        padding: const EdgeInsets.symmetric(
-            horizontal: AppDimensions.paddingMedium,
-            vertical: AppDimensions.paddingMedium),
-        child: Text(defaultOrderTitle,
-            style: AppTextStyles.bodyText1.copyWith(color: AppColors.white)),
+    return GestureDetector(
+      onTapDown: _onTapDown,
+      onTapUp: _onTapUp,
+      onTap: () {},
+      child: SizedBox(
+        height: 80,
+        width: ScreenHelper.width(context) * 0.4,
+        child: ScaleTransition(
+          scale: _scaleAnimation,
+          child: Card(
+            margin: const EdgeInsets.symmetric(
+                horizontal: AppDimensions.marginMedium, vertical: 0),
+            elevation: 5,
+            shape: Theme.of(context).cardTheme.shape,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                  horizontal: AppDimensions.paddingSmall, vertical: 0),
+              child: Center(
+                child: Text(
+                  '${widget.defaultOrderTitle}\n(${widget.count})',
+                  textAlign: TextAlign.center,
+                  style: AppTextStyles.bodyText1.copyWith(
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }

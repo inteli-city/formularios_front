70b4e736164f89afa93ce732b9c10dad0369b10d
Yuji Arima <yujiarima@hotmail.com>
2024-04-29 13:01:20 -0300

    serch_filter_tab onpressed

--- lib/app/presentation/widgets/search_filter_tab.dart
+++ lib/app/presentation/widgets/search_filter_tab.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:formularios_front/app/presentation/stores/providers/filtered_data_provider.dart';
+import 'package:formularios_front/app/presentation/widgets/filter_order_dialog_widget.dart';
 import 'package:formularios_front/app/shared/helpers/utils/screen_helper.dart';
 import 'package:formularios_front/app/shared/themes/app_dimensions.dart';
+import 'package:provider/provider.dart';
 
 class SearchFilterTab extends StatefulWidget {
   const SearchFilterTab({super.key});
@@ -12,14 +15,27 @@ class SearchFilterTab extends StatefulWidget {
 class _SearchFilterTabState extends State<SearchFilterTab> {
   @override
   Widget build(BuildContext context) {
-    return Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
-      _buildSearchField(context),
-      Icon(
-        Icons.filter_alt_sharp,
-        size: AppDimensions.iconMedium,
-        color: Theme.of(context).colorScheme.primary,
-      )
-    ]);
+    var forms = context.read<FilteredDataProvider>().filteredForms;
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        _buildSearchField(context),
+        IconButton(
+          onPressed: () async {
+            showDialog(
+              context: context,
+              builder: (context) => FilterOrderDialogWidget(
+                forms: forms,
+              ),
+            );
+          },
+          icon: const Icon(Icons.filter_list_rounded),
+          iconSize: AppDimensions.iconMedium,
+          color: Theme.of(context).colorScheme.primary,
+        )
+      ],
+    );
   }
 
   Widget _buildSearchField(BuildContext context) {

92781262eb0bac51261a118b28fd2b6a994c8474
Yuji Arima <yujiarima@hotmail.com>
2024-05-08 21:42:22 -0300

    sort_forms_dialog unity test

--- lib/app/presentation/widgets/dialogs/sort_forms_dialog.dart
+++ lib/app/presentation/widgets/dialogs/sort_forms_dialog.dart
@@ -18,7 +18,7 @@ class _SortFormsDialog extends State<SortFormsDialog> {
   var controller = Modular.get<SortFormsController>();
   @override
   Widget build(BuildContext context) {
-    var formProvider = context.watch<FormUserProvider>();
+    var formProvider = context.read<FormUserProvider>();
     return Container(
       padding: const EdgeInsets.only(top: AppDimensions.paddingExtraLarge),
       height: 300,

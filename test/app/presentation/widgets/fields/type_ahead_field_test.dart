58196791969f7779a7ddb43296673c8e8cf36468
Yuji Arima <yujiarima@hotmail.com>
2024-06-05 11:15:34 -0300

    test : type_ahead_field

---
+++ test/app/presentation/widgets/fields/type_ahead_field_test.dart
@@ -0,0 +1,188 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:formularios_front/app/app_module.dart';
+import 'package:formularios_front/app/domain/entities/field_entity.dart';
+import 'package:formularios_front/app/presentation/form/controllers/form_controller.dart';
+import 'package:formularios_front/app/presentation/form/widgets/fields/type_ahead_field.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import 'type_ahead_field_test.mocks.dart';
+
+@GenerateMocks([FormController])
+void main() {
+  FormController formController = MockFormController();
+  Modular.bindModule(AppModule());
+  Modular.bindModule(HomeModule());
+  Modular.replaceInstance<FormController>(formController);
+
+  group('CustomTypeAheadFormField Tests', () {
+    setUp(() {
+      when(formController.getIsSendingForm()).thenReturn(true);
+    });
+    testWidgets('Displays initial value and placeholder',
+        (WidgetTester tester) async {
+      final field = TypeAheadFieldEntity(
+        placeholder: 'Enter text',
+        key: 'testTypeAhead',
+        isRequired: true,
+        options: ['Option 1', 'Option 2'],
+        value: 'Initial value',
+      );
+
+      await tester.pumpWidget(
+        MaterialApp(
+          home: Scaffold(
+            body: CustomTypeAheadFormField(
+              field: field,
+              onChanged: (value) {},
+            ),
+          ),
+        ),
+      );
+
+      expect(find.text('Initial value'), findsOneWidget);
+      expect(find.text('Enter text'), findsOneWidget);
+    });
+
+    testWidgets('Displays suggestions when typing',
+        (WidgetTester tester) async {
+      final field = TypeAheadFieldEntity(
+        placeholder: 'Enter text',
+        key: 'testTypeAhead',
+        isRequired: true,
+        options: ['Option 1', 'Option 2'],
+      );
+
+      await tester.pumpWidget(
+        MaterialApp(
+          home: Scaffold(
+            body: CustomTypeAheadFormField(
+              field: field,
+              onChanged: (value) {},
+            ),
+          ),
+        ),
+      );
+
+      await tester.enterText(find.byType(TextFormField), 'Option');
+      await tester.pumpAndSettle();
+
+      expect(find.text('Option 1'), findsOneWidget);
+      expect(find.text('Option 2'), findsOneWidget);
+    });
+
+    testWidgets('Selects a suggestion and updates the controller',
+        (WidgetTester tester) async {
+      final field = TypeAheadFieldEntity(
+        placeholder: 'Enter text',
+        key: 'testTypeAhead',
+        isRequired: true,
+        options: ['Option 1', 'Option 2'],
+      );
+
+      await tester.pumpWidget(
+        MaterialApp(
+          home: Scaffold(
+            body: CustomTypeAheadFormField(
+              field: field,
+              onChanged: (value) {},
+            ),
+          ),
+        ),
+      );
+
+      await tester.pumpAndSettle();
+      await tester.tap(find.text('Option 1'));
+      await tester.pumpAndSettle();
+
+      expect(find.text('Option 1'), findsOneWidget);
+    });
+
+    testWidgets(
+        'Displays required field error when no text is entered and send option is clicked',
+        (WidgetTester tester) async {
+      final field = TypeAheadFieldEntity(
+        placeholder: 'Enter text',
+        key: 'testTypeAhead',
+        isRequired: true,
+        options: ['Option 1', 'Option 2'],
+      );
+      final formKey = GlobalKey<FormState>();
+
+      await tester.pumpWidget(
+        MaterialApp(
+          home: Scaffold(
+            body: Form(
+              key: formKey,
+              child: Column(
+                children: [
+                  CustomTypeAheadFormField(
+                    field: field,
+                    onChanged: (value) {},
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      formController.setIsSendingForm(true);
+                      formKey.currentState!.validate();
+                    },
+                    child: const Text('Enviar'),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      );
+
+      await tester.tap(find.text('Enviar'));
+      await tester.pumpAndSettle();
+
+      expect(find.text('Este campo é obrigatório'), findsOneWidget);
+    });
+
+    testWidgets(
+        'Does not display required field error when text is entered and send option is clicked',
+        (WidgetTester tester) async {
+      final field = TypeAheadFieldEntity(
+        placeholder: 'Enter text',
+        key: 'testTypeAhead',
+        isRequired: true,
+        options: ['Option 1', 'Option 2'],
+      );
+      final formKey = GlobalKey<FormState>();
+
+      await tester.pumpWidget(
+        MaterialApp(
+          home: Scaffold(
+            body: Form(
+              key: formKey,
+              child: Column(
+                children: [
+                  CustomTypeAheadFormField(
+                    field: field,
+                    onChanged: (value) {},
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      formController.setIsSendingForm(true);
+                      formKey.currentState!.validate();
+                    },
+                    child: const Text('Enviar'),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      );
+
+      await tester.enterText(find.byType(TextFormField), 'Option 1');
+      await tester.tap(find.text('Enviar'));
+      await tester.pumpAndSettle();
+
+      expect(find.text('Este campo é obrigatório'), findsNothing);
+    });
+  });
+}

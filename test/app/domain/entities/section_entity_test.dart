3ae672791b7fb7527b301c78d1d52045ade55b91
Yuji Arima <yujiarima@hotmail.com>
2024-06-25 15:01:35 -0300

    test: section_entity add test for failure

--- test/app/domain/entities/section_entity_test.dart
+++ test/app/domain/entities/section_entity_test.dart
@@ -4,19 +4,22 @@ import 'package:formularios_front/app/domain/entities/section_entity.dart';
 import 'package:formularios_front/app/domain/enum/field_type_enum.dart';
 
 void main() {
-  test('Section Entity Test', () {
+  test('should return a valid Section Entity', () {
     final FieldEntity fieldEntity0 = TextFieldEntity(
       fieldType: FieldTypeEnum.DATE_FIELD,
       placeholder: 'placeholder',
       isRequired: true,
       key: 'key',
     );
-    final sectionFieldEntity = SectionEntity(
-        sectionId: 'section-id',
-        fields: [fieldEntity0]);
+    final sectionFieldEntity =
+        SectionEntity(sectionId: 'section-id', fields: [fieldEntity0]);
 
     expect(sectionFieldEntity.sectionId, 'section-id');
-    expect(
-        sectionFieldEntity.fields, [fieldEntity0]);
+    expect(sectionFieldEntity.fields, [fieldEntity0]);
+  });
+
+  test('should throw exception for invalid Section Entity', () {
+    expect(() => SectionEntity(sectionId: 'section-id', fields: []),
+        throwsException);
   });
 }

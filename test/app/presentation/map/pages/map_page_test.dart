c20ec45ac76a9ab0bc0da2784b570e1b2e1c3213
Yuji Arima <yujiarima@hotmail.com>
2024-06-27 16:03:29 -0300

    test: map_page

---
+++ test/app/presentation/map/pages/map_page_test.dart
@@ -0,0 +1,37 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:formularios_front/app/app_module.dart';
+import 'package:formularios_front/app/presentation/home/stores/forms_provider.dart';
+import 'package:formularios_front/app/presentation/map/pages/map_page.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:flutter/material.dart';
+
+import 'map_page_test.mocks.dart';
+
+@GenerateMocks([FormsProvider])
+void main() {
+  FormsProvider mockFormsProvider = MockFormsProvider();
+  Modular.bindModule(HomeModule());
+
+  Modular.replaceInstance<FormsProvider>(mockFormsProvider);
+
+  when(mockFormsProvider.locations).thenReturn([]);
+
+  testWidgets('MapPage displays Google Map with markers',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: Scaffold(
+          body: MapPage(),
+        ),
+      ),
+    );
+
+    await tester.pumpAndSettle();
+
+    debugPrint(tester.element(find.byType(MapPage)).toStringDeep());
+    expect(find.byType(GoogleMap), findsOneWidget);
+  });
+}

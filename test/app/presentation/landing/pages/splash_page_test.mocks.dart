0c4d0ca55d7f9d1ab6ed84cbb9bfb42bdcaaa2bf
Yuji Arima <yujiarima@hotmail.com>
2024-07-16 13:30:26 -0300

    test: adds other form status in formcard

--- test/app/presentation/landing/pages/splash_page_test.mocks.dart
+++ test/app/presentation/landing/pages/splash_page_test.mocks.dart
@@ -3,16 +3,12 @@
 // Do not manually edit this file.
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'dart:async' as _i5;
-import 'dart:ui' as _i7;
+import 'dart:async' as _i3;
+import 'dart:ui' as _i4;
 
-import 'package:flutter/material.dart' as _i6;
-import 'package:flutter_modular/flutter_modular.dart' as _i2;
-import 'package:flutter_modular/src/presenter/models/route.dart' as _i4;
 import 'package:formularios_front/app/presentation/landing/controllers/splash_controller.dart'
-    as _i8;
+    as _i2;
 import 'package:mockito/mockito.dart' as _i1;
-import 'package:mockito/src/dummies.dart' as _i3;
 
 // ignore_for_file: type=lint
 // ignore_for_file: avoid_redundant_argument_values
@@ -27,214 +23,10 @@ import 'package:mockito/src/dummies.dart' as _i3;
 // ignore_for_file: camel_case_types
 // ignore_for_file: subtype_of_sealed_class
 
-/// A class which mocks [IModularNavigator].
-///
-/// See the documentation for Mockito's code generation for more information.
-class MockIModularNavigator extends _i1.Mock implements _i2.IModularNavigator {
-  MockIModularNavigator() {
-    _i1.throwOnMissingStub(this);
-  }
-
-  @override
-  String get path => (super.noSuchMethod(
-        Invocation.getter(#path),
-        returnValue: _i3.dummyValue<String>(
-          this,
-          Invocation.getter(#path),
-        ),
-      ) as String);
-
-  @override
-  List<_i4.ParallelRoute<dynamic>> get navigateHistory => (super.noSuchMethod(
-        Invocation.getter(#navigateHistory),
-        returnValue: <_i4.ParallelRoute<dynamic>>[],
-      ) as List<_i4.ParallelRoute<dynamic>>);
-
-  @override
-  _i5.Future<T?> push<T extends Object?>(_i6.Route<T>? route) =>
-      (super.noSuchMethod(
-        Invocation.method(
-          #push,
-          [route],
-        ),
-        returnValue: _i5.Future<T?>.value(),
-      ) as _i5.Future<T?>);
-
-  @override
-  _i5.Future<T?> popAndPushNamed<T extends Object?, TO extends Object?>(
-    String? routeName, {
-    TO? result,
-    Object? arguments,
-    bool? forRoot = false,
-  }) =>
-      (super.noSuchMethod(
-        Invocation.method(
-          #popAndPushNamed,
-          [routeName],
-          {
-            #result: result,
-            #arguments: arguments,
-            #forRoot: forRoot,
-          },
-        ),
-        returnValue: _i5.Future<T?>.value(),
-      ) as _i5.Future<T?>);
-
-  @override
-  _i5.Future<T?> pushNamed<T extends Object?>(
-    String? routeName, {
-    Object? arguments,
-    bool? forRoot = false,
-  }) =>
-      (super.noSuchMethod(
-        Invocation.method(
-          #pushNamed,
-          [routeName],
-          {
-            #arguments: arguments,
-            #forRoot: forRoot,
-          },
-        ),
-        returnValue: _i5.Future<T?>.value(),
-      ) as _i5.Future<T?>);
-
-  @override
-  _i5.Future<T?> pushNamedAndRemoveUntil<T extends Object?>(
-    String? newRouteName,
-    bool Function(_i6.Route<dynamic>)? predicate, {
-    Object? arguments,
-    bool? forRoot = false,
-  }) =>
-      (super.noSuchMethod(
-        Invocation.method(
-          #pushNamedAndRemoveUntil,
-          [
-            newRouteName,
-            predicate,
-          ],
-          {
-            #arguments: arguments,
-            #forRoot: forRoot,
-          },
-        ),
-        returnValue: _i5.Future<T?>.value(),
-      ) as _i5.Future<T?>);
-
-  @override
-  _i5.Future<T?> pushReplacementNamed<T extends Object?, TO extends Object?>(
-    String? routeName, {
-    TO? result,
-    Object? arguments,
-    bool? forRoot = false,
-  }) =>
-      (super.noSuchMethod(
-        Invocation.method(
-          #pushReplacementNamed,
-          [routeName],
-          {
-            #result: result,
-            #arguments: arguments,
-            #forRoot: forRoot,
-          },
-        ),
-        returnValue: _i5.Future<T?>.value(),
-      ) as _i5.Future<T?>);
-
-  @override
-  void pop<T extends Object?>([T? result]) => super.noSuchMethod(
-        Invocation.method(
-          #pop,
-          [result],
-        ),
-        returnValueForMissingStub: null,
-      );
-
-  @override
-  bool canPop() => (super.noSuchMethod(
-        Invocation.method(
-          #canPop,
-          [],
-        ),
-        returnValue: false,
-      ) as bool);
-
-  @override
-  _i5.Future<bool> maybePop<T extends Object?>([T? result]) =>
-      (super.noSuchMethod(
-        Invocation.method(
-          #maybePop,
-          [result],
-        ),
-        returnValue: _i5.Future<bool>.value(false),
-      ) as _i5.Future<bool>);
-
-  @override
-  void popUntil(bool Function(_i6.Route<dynamic>)? predicate) =>
-      super.noSuchMethod(
-        Invocation.method(
-          #popUntil,
-          [predicate],
-        ),
-        returnValueForMissingStub: null,
-      );
-
-  @override
-  void navigate(
-    String? path, {
-    dynamic arguments,
-  }) =>
-      super.noSuchMethod(
-        Invocation.method(
-          #navigate,
-          [path],
-          {#arguments: arguments},
-        ),
-        returnValueForMissingStub: null,
-      );
-
-  @override
-  void setObservers(List<_i6.NavigatorObserver>? navigatorObservers) =>
-      super.noSuchMethod(
-        Invocation.method(
-          #setObservers,
-          [navigatorObservers],
-        ),
-        returnValueForMissingStub: null,
-      );
-
-  @override
-  void setNavigatorKey(_i6.GlobalKey<_i6.NavigatorState>? navigatorkey) =>
-      super.noSuchMethod(
-        Invocation.method(
-          #setNavigatorKey,
-          [navigatorkey],
-        ),
-        returnValueForMissingStub: null,
-      );
-
-  @override
-  void addListener(_i7.VoidCallback? listener) => super.noSuchMethod(
-        Invocation.method(
-          #addListener,
-          [listener],
-        ),
-        returnValueForMissingStub: null,
-      );
-
-  @override
-  void removeListener(_i7.VoidCallback? listener) => super.noSuchMethod(
-        Invocation.method(
-          #removeListener,
-          [listener],
-        ),
-        returnValueForMissingStub: null,
-      );
-}
-
 /// A class which mocks [SplashController].
 ///
 /// See the documentation for Mockito's code generation for more information.
-class MockSplashController extends _i1.Mock implements _i8.SplashController {
+class MockSplashController extends _i1.Mock implements _i2.SplashController {
   MockSplashController() {
     _i1.throwOnMissingStub(this);
   }
@@ -246,17 +38,17 @@ class MockSplashController extends _i1.Mock implements _i8.SplashController {
       ) as bool);
 
   @override
-  _i5.Future<void> loadUser() => (super.noSuchMethod(
+  _i3.Future<void> loadUser() => (super.noSuchMethod(
         Invocation.method(
           #loadUser,
           [],
         ),
-        returnValue: _i5.Future<void>.value(),
-        returnValueForMissingStub: _i5.Future<void>.value(),
-      ) as _i5.Future<void>);
+        returnValue: _i3.Future<void>.value(),
+        returnValueForMissingStub: _i3.Future<void>.value(),
+      ) as _i3.Future<void>);
 
   @override
-  void addListener(_i7.VoidCallback? listener) => super.noSuchMethod(
+  void addListener(_i4.VoidCallback? listener) => super.noSuchMethod(
         Invocation.method(
           #addListener,
           [listener],
@@ -265,7 +57,7 @@ class MockSplashController extends _i1.Mock implements _i8.SplashController {
       );
 
   @override
-  void removeListener(_i7.VoidCallback? listener) => super.noSuchMethod(
+  void removeListener(_i4.VoidCallback? listener) => super.noSuchMethod(
         Invocation.method(
           #removeListener,
           [listener],

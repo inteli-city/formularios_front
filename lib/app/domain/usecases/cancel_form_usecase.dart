591b36d13cfb3679663d333e702066c074bce1bc
Yuji Arima <yujiarima@hotmail.com>
2024-07-22 14:11:51 -0300

    cancelForm usecase fix

--- lib/app/domain/usecases/cancel_form_usecase.dart
+++ lib/app/domain/usecases/cancel_form_usecase.dart
@@ -1,6 +1,5 @@
 import 'package:dartz/dartz.dart';
 import 'package:formularios_front/app/domain/entities/justificative_entity.dart';
-import 'package:formularios_front/app/domain/enum/form_status_enum.dart';
 import 'package:gates_microapp_flutter/shared/helpers/errors/errors.dart';
 import 'package:formularios_front/app/domain/repositories/form_repository.dart';
 
@@ -18,9 +17,6 @@ class CancelFormUseCase implements ICancelFormUseCase {
   Future<Either<Failure, JustificativeEntity>> call(
       {required JustificativeEntity justificative,
       required String formId}) async {
-    await repository.updateFormStatus(
-        status: FormStatusEnum.CANCELED, formId: formId);
-
     return await repository.cancelForm(
         formId: formId, justificative: justificative);
   }
